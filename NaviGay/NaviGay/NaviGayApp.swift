4e651081efcd27d16ebff8a541999714e7c0486b
Zasenko <dmitry.zasenko@gmail.com>
2023-03-21 21:26:18 +0100

    add TestView in Project

--- NaviGay/NaviGay/NaviGayApp.swift
+++ NaviGay/NaviGay/NaviGayApp.swift
@@ -13,7 +13,7 @@ struct NaviGayApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TestView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
